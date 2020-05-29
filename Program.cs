8fd182fb4ecda92e3d364405a187a4b919f13d6e
jderfer0979@gmail.com <jderfer0979@gmail.com>
2020-05-29 18:11:10 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HomeworkProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string first = "Jeremy";
+            string last = "Langenderfer";
+            string hobby = "travel";
+            Console.WriteLine("My name is " + first + " " + last + " and I love to " + hobby); 
+        }
+    }
+}
